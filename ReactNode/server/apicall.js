20736c1bd30a635123c71e26df29e9df3aa67931
Jamal Parker <jrparker1@aggies.ncat.edu>
2025-04-05 15:31:16 -0400

    add all properties

--- ReactNode/server/apicall.js
+++ ReactNode/server/apicall.js
@@ -20,6 +20,17 @@ class WeatherData {
         this._airquality_so2 = null;
         this._airquality_pm2_5 = null;
         this._airquality_pm10 = null;
+        this._windspeed = null;
+        this._winddegree = null;
+        this._winddir = null;
+        this._pressure = null;
+        this._precip = null;
+        this._humidity = null;
+        this._cloudcover = null;
+        this._feelslike = null;
+        this._uvindex = null;
+        this._visibility = null;
+        this._isday = null;
 
         // Make the API call
         this.fetchWeatherData();
@@ -65,6 +76,43 @@ class WeatherData {
 
     get AirQuality_PM10() { return this._airquality_pm10; }
     set AirQuality_PM10(data) { this._airquality_pm10 = data.air_quality.pm10; }
+
+    get AirQuality_PM10() { return this._airquality_pm10; }
+    set AirQuality_PM10(data) { this._airquality_pm10 = data.air_quality.pm10; }
+
+    get WindSpeed() { return this._windspeed; }
+    set WindSpeed(data) { this._windspeed = data.wind_speed; }
+
+    get WindDegree() { return this._winddegree; }
+    set WindDegree(data) { this._winddegree = data.wind_degree; }
+
+    get WindDir() { return this._winddir; }
+    set WindDir(data) { this._winddir = data.wind_dir; }
+
+    get Pressure() { return this._pressure; }
+    set Pressure(data) { this._pressure = data.pressure; }
+
+    get Precip() { return this._precip; }
+    set Precip(data) { this._precip = data.precip; }
+
+    get Humidity() { return this._humidity; }
+    set Humidity(data) { this._humidity = data.humidity; }
+
+    get CloudCover() { return this._cloudcover; }
+    set CloudCover(data) { this._cloudcover = data.cloudcover; }
+
+    get FeelsLike() { return this._feelslike; }
+    set FeelsLike(data) { this._feelslike = data.feelslike; }
+
+    get UVIndex() { return this._uvindex; }
+    set UVIndex(data) { this._uvindex = data.uv_index; }
+
+    get Visibility() { return this._visibility; }
+    set Visibility(data) { this._visibility = data.visibility; }
+
+    get IsDay() { return this._isday; }
+    set IsDay(data) { this._isday = data.is_day; }
+
     // #endregion
 
     // #region methods 
@@ -122,11 +170,6 @@ class WeatherData {
             this.Visibility = data;
             this.IsDay = data;
 
-
-            // Log the weather data
-             console.log('Weather Data:', data);
-             console.log('Temperature:', data.temperature);
-             console.log('Weather Description:', data.weather_descriptions[0]); // Example: "Partly Cloudy"
         } catch (error) {
             console.error('Error fetching weather data:', error);
         }
@@ -150,4 +193,15 @@ setTimeout(() => {
      console.log('AirQuality_SO2 after API call:', weather.AirQuality_SO2);
      console.log('AirQuality_PM2_5 after API call:', weather.AirQuality_PM2_5);
      console.log('AirQuality_PM10 after API call:', weather.AirQuality_PM10);
-}, 30000);
+     console.log('WindSpeed after API call:', weather.WindSpeed);
+     console.log('WindDegree after API call:', weather.WindDegree);
+     console.log('WindDir after API call:', weather.WindDir);
+     console.log('Pressure after API call:', weather.Pressure);
+     console.log('Precip after API call:', weather.Precip);
+     console.log('Humidity after API call:', weather.Humidity);
+     console.log('CloudCover after API call:', weather.CloudCover);
+     console.log('FeelsLike after API call:', weather.FeelsLike);
+     console.log('UVIndex after API call:', weather.UVIndex);
+     console.log('Visibility after API call:', weather.Visibility);
+     console.log('IsDay after API call:', weather.IsDay);
+}, 10000);
